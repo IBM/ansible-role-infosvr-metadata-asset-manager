09d610b9791b4ac2a1c577b3c50d37d83e3c5e0c
Christopher Grote <chris@thegrotes.net>
2018-08-15 13:39:43 +0100

    Adds module and helper utils for generating OSH schema files

---
+++ library/imam_generate_osh.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+
+###
+# Copyright 2018 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: imam_generate_osh
+
+short_description: Generates OSH schema files for data files
+
+description:
+  - Generates OSH schema files for data files.
+  - Based on a SQL (DDL) input of one or more CREATE TABLE statements.
+
+version_added: "2.4"
+
+author:
+  - Christopher Grote (@cmgrote)
+
+options:
+  ddl:
+    description:
+      - Name of the SQL file containing the CREATE TABLE statements
+    required: true
+    type: path
+  structure:
+    description:
+      - Structural definition to use for the OSH schema
+      - For example: "file_format='delimited', header='false'"
+    required: true
+    type: str
+  recordfmt:
+    description:
+      - Formatting to use for the records defined by the OSH schema
+      - For example: "delim='|', final_delim=end, null_field='', charset=UTF-8"
+    required: true
+    type: str
+  table:
+    description:
+      - The table for which to generate an OSH schema
+    required: true
+    type: str
+  dest:
+    description:
+      - The target file into which to write the generated OSH schema
+    required: true
+    type: path
+'''
+
+EXAMPLES = '''
+- name: generate OSH schema for MY_TABLE
+  imam_generate_osh:
+    ddl: mydb.sql
+    structure: "file_format: 'delimited', header: 'false'"
+    recordfmt: "delim='|', final_delim=end, null_field='', charset=UTF-8"
+    table: MY_TABLE
+    dest: /some/location/MY_TABLE.csv.osh
+'''
+
+RETURN = '''
+warnings:
+  description: A list of any warnings regarding the OSH schema generation (eg. defaulted types)
+  returned: always
+  type: list
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_bytes, to_native
+from ansible.module_utils.infosvr_types import getCreateTableStatementsFromDDL, getColumnDefinitionsFromCreateTableStatement, convertColumnDefinitionToOSHSchemaFieldDefinition
+import os
+import os.path
+import tempfile
+
+
+def main():
+
+    module_args = dict(
+        ddl=dict(type='path', required=True),
+        structure=dict(type='str', required=True),
+        recordfmt=dict(type='str', required=True),
+        table=dict(type='str', required=True),
+        dest=dict(type='path', required=True),
+        unsafe_writes=dict(type='bool', required=False, default=False)
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    result = dict(
+        changed=False,
+        warnings=[]
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        return result
+
+    ddl = module.params['ddl']
+
+    if os.path.isdir(ddl):
+        module.fail_json(rc=256, msg='DDL %s is a directory !' % ddl)
+
+    ddl_exists = os.path.exists(ddl)
+    if not ddl_exists:
+        module.fail_json(rc=257, msg='DDL %s does not exist !' % ddl)
+
+    f = open(to_bytes(ddl), 'rb')
+    ddlString = f.read()
+    f.close()
+
+    aCreateTbls = getCreateTableStatementsFromDDL(ddlString);
+    tablesToFields = {};
+    for table in aCreateTbls:
+        tblObj = getColumnDefinitionsFromCreateTableStatement(table)
+        tablesToFields[tblObj['table']] = tblObj['columns']
+
+    oshSchema = "// FileStructure: " + module.params['structure'] + "\n"
+    oshSchema += "record { " + module.params['recordfmt'] + " } ("
+
+    ucaseTblName = tableName.upper()
+    if not ucaseTblName in tablesToFields:
+        module.fail_json(rc=1, msg='Unable to find table name: %s' % ucaseTblName)
+
+    fieldDefinitions = tablesToFields[ucaseTblName]
+    for field in fieldDefinitions:
+        colDefn = convertColumnDefinitionToOSHSchemaFieldDefinition(result, field)
+        oshSchema += "\n    " + colDefn;
+
+    oshSchema += "\n)"
+
+    fs.writeFileSync(sidecarName, oshSchema, 'utf8');
+
+    # Write temporary file with the OSH output,
+    # and then move to specified dest location
+    try:
+        tmpfd, tmpfile = tempfile.mkstemp()
+        f = os.fdopen(tmpfd, 'wb')
+        f.write(oshSchema)
+        f.close()
+    except IOError:
+        module.fail_json(msg='Unable to create temporary file to output OSH schema', **result)
+
+    # Checksumming to identify change...
+    checksum_src = module.sha1(tmpfile)
+    checksum_dest = None
+    dest = module.params['dest']
+    b_dest = to_bytes(dest, errors='surrogate_or_strict')
+    if os.access(b_dest, os.R_OK):
+        checksum_dest = module.sha1(dest)
+
+    # If the file does not already exist and/or checksums are different,
+    # move the new file over the old one and mark it as changed; otherwise
+    # leave the original file (delete the tmpfile) and that there was no change
+    if checksum_src != checksum_dest:
+        module.atomic_move(tmpfile,
+                        to_native(os.path.realpath(b_dest), errors='surrogate_or_strict'),
+                        unsafe_writes=module.params['unsafe_writes'])
+        result['changed'] = True
+    else:
+        os.unlink(tmpfile)
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ module_utils/osh_types.py
@@ -0,0 +1,114 @@
+###
+# Copyright 2018 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+"""
+This module adds generic utility functions for translating between DDL and OSH data types
+"""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import re
+
+
+sql_type_to_osh_type = {
+    "CHAR": "string",
+    "DATE": "date",
+    "DECIMAL": "decimal",
+    "NUMBER": "decimal",
+    "INTEGER": "int64",
+    "TIME": "time",
+    "TIMESTAMP": "timestamp",
+    "VARCHAR": "string",
+    "NVARCHAR2": "string"
+}
+
+
+def getOSHSchemaTypeForSQLType(result, sqlType):
+    # http://www.ibm.com/support/knowledgecenter/en/SSZJPZ_11.5.0/com.ibm.swg.im.iis.ds.parjob.adref.doc/topics/r_deeadvrf_ImportExport_Properties.html
+    sqlTypeAlone = sqlType
+    sqlTypeLength = ""
+    if sqlType.index("(") > 0:
+        sqlTypeAlone = sqlType[0:sqlType.index("(")]
+        sqlTypeLength = sqlType[(sqlType.index("(") + 1):sqlType.index(")")]
+    ucaseSQL = sqlTypeAlone.upper()
+    oshSchemaType = "string"
+    if ucaseSQL in sql_type_to_osh_type:
+        oshSchemaType = sql_type_to_osh_type[ucaseSQL]
+        if sqlTypeLength != "":
+            if oshSchemaType == "string":
+                oshSchemaType += "[max=" + sqlTypeLength + "]"
+            else:
+                oshSchemaType += "[" + sqlTypeLength + "]"
+    else:
+        result['warnings'].append("Unsupported SQL data type: " + ucaseSQL)
+    return oshSchemaType
+
+
+def getCreateTableStatementsFromDDL(ddlString):
+    tblStatments = []
+#    ddlString = fs.readFileSync(ddlFile, 'utf8')
+    aLinesDDL = ddlString.split("\n")
+    currentTableDef = "";
+    idx = 0
+    regex = re.compile(r"\s\s+")
+    for line in aLinesDDL:
+        line = line.strip().upper();
+        if line.startswith("CREATE TABLE"):
+            if currentTableDef != "":
+                tblStatments.push(currentTableDef)
+            currentTableDef = regex.sub(string=line, repl=' ')
+        else if not line.startswith("--") and line != "":
+            currentTableDef += regex.sub(string=line, repl=' ')
+        # Also ensure the very last create table statement is included
+        if aLinesDDL.length == (idx + 1) and currentTableDef.startswith("CREATE TABLE"):
+            tblStatments.append(currentTableDef)
+        idx += 1
+    return tblStatments
+
+
+def getColumnDefinitionsFromCreateTableStatement(ddlCreateTable):
+    iDefnStart = ddlCreateTable.index("(")
+    iDefnEnd = ddlCreateTable.rindex(")")
+    tblName = ddlCreateTable[(len("CREATE TABLE ")):iDefnStart]
+    tblDefn = ddlCreateTable[(iDefnStart + 1):iDefnEnd]
+    aNaiveColDefns = tblDefn.split(",") # not quite so simple, since DECIMAL(5,2) will be split in the middle...
+    aActualColDefns = []
+    idx = 0
+    for candidateCol in aNaiveColDefns:
+        if candidateCol.index("(") > 0:
+            # If we find an opening (, then greedily consume into the same column until we find the corresponding closing )
+            while candidateCol.index(")") < 0:
+                candidateCol += "," + aNaiveColDefns[++idx]
+        if not candidateCol.startswith("PRIMARY KEY"):
+            aActualColDefns.append(candidateCol)
+        idx += 1
+    return { "table": tblName, "columns": aActualColDefns }
+
+
+def convertColumnDefinitionToOSHSchemaFieldDefinition(result, ddlCol):
+    remainder = ddlCol
+    colName = remainder[0:remainder.index(" ")]
+    remainder = remainder[len(colName):].strip()
+    colType = remainder
+    extras = ""
+    if remainder.index(" ") > 0:
+        colType = remainder[0:remainder.indexOf(" ")]
+        extras = remainder[len(colType):].strip()
+    if extras == "NOT NULL":
+        extras = "not nullable"
+    else:
+        extras = "nullable"
+    return colName + ": " + extras + " " + getOSHSchemaTypeForSQLType(result, colType) + ";";
